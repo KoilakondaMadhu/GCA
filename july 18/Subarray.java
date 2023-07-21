4519a6d4dfa5ca8de85e7609530fab42e9cb129f
MADHUVEER <111329658+KoilakondaMadhu@users.noreply.github.com>
2023-07-21 11:42:57 +0530

    Update Subarray.java

--- july 18/Subarray.java
+++ july 18/Subarray.java
@@ -1,21 +1,31 @@
-importjava.util.Scanner*;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
-public class Subarry {
-    public void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        int n;
-        n = scanner.nextInt();
-        int[] arr = new int[n];
-        for(int i =0; i<n;i++) arr[i] = scanner.nextInt();
-        int ct = 0;
-        for(int i =0;i < n ; i++){
-            int sum =0;
-            for(int k = i;k< n;k++){
-                sum = sum + arr[j];
-                if(sum < 0) ct += 1;
+public class Subarray {
 
-            }
+    public static void main(String[] args) {
+        /* Enter your code here. Read input 
+        from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        for (int i =0;i < n;i++){
+            a[i] = sc.nextInt();
         }
+        int k = 0;
+        int sum =0;
+        for (int i =0;i<n;i++){
+            for(int j =i;j<n;j++){
+                sum += a[j];
+                if(sum < 0){
+                    k++;
+                }
+            }
+            sum =0;
+        }
+        System.out.println(k);
     }
-    
 }
