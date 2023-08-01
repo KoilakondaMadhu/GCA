f0e9224eccaf62b6965925c10343f5532ad7b540
MADHUVEER <111329658+KoilakondaMadhu@users.noreply.github.com>
2023-08-01 20:03:48 +0530

    Add files via upload

---
+++ august 1/script.js
@@ -0,0 +1,49 @@
+// a = 10;
+
+//  const x =a;
+//  a = 20
+// console.log(a);
+
+// // output 20 so mani pulated
+// ==========================================================
+
+// let arr =["adf","sdaf"];
+// console.log(arr);
+// arr.push("asd");
+// console.log(arr);
+// arr.pop();                   // pop removes only last element
+// console.log(arr);
+
+
+// ======================================================================
+// ================Splice==================================
+// let fru =["mango","apple","orange","kiwi","banana"]
+// console.log(fru.splice(0,3))
+// fas = fru;
+// console.log(fas)
+
+// output
+//  ['mango', 'apple', 'orange']
+//  ['kiwi', 'banana']
+
+// remove part of array
+
+// ++++++++++++++++++++++slice++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// let fru =["mango","apple","orange","kiwi","banana"]
+// console.log(fru.slice(0,3))
+// fas = fru;
+// console.log(fas)
+
+// output
+
+//  ['mango', 'apple', 'orange']
+//  ['mango', 'apple', 'orange', 'kiwi', 'banana']
+// // keep part of Array
+
+const per = new Object();
+console.log(per);
+per.firstName:"veer",
+per.lastName:"madhu";
+console.log(per);
